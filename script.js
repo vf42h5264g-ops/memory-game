5c8ef6cdbac72adf2fd59912697019347d7cf088
vf42h5264g-ops <vf42h5264g@privaterelay.appleid.com>
2026-01-10 10:42:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ const startBtn = document.getElementById("startBtn");
 const startScreen = document.getElementById("startScreen");
 const gameScreen = document.getElementById("gameScreen");
 
+function startGame() {
+  console.log("startGame called"); // デバッグ用
+  createCards();   // ← カード生成
+}
+
 startBtn.addEventListener("click", () => {
   startScreen.classList.add("hidden");
   gameScreen.classList.remove("hidden");
-  alert("ゲーム開始！");
+  startGame ();
 });
+
+function createCards() {
+  gameScreen.innerHTML = "";
+
+  for (let i = 1; i <= 6; i++) {
+    const card = document.createElement("img");
+    card.src = "back.jpg";
+    card.className = "card";
+    gameScreen.appendChild(card);
+  }
+}
